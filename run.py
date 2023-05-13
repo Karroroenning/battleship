88468ae724a54a42824b6a3db205b0c369490704
karolina Rönning <Karroroenning@gmail.com>
2023-05-13 09:56:41 +0000

    Added start screen and a enter the game input

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#import random
+
+def menu():
+    print(
+    """
+    __  ____ ___ ___ _    ____ ____ _  _ _ ___  ____
+    |__] |__|  |   |  |    |___ [__  |__| | |__] [__
+    |__] |  |  |   |  |___ |___ ___] |  | | |    ___]\n
+
+    Press Enter to start
+
+    """
+        )       
+    input_val = input('')
+     
+
+
+#class Gameboard:
+   # def __init__(self, board):
+    #    self.board = board
+
+   # def get_letters_to_numbers():
+     #   letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+       # return letters_to_numbers
+
+   # def print_board(self):
+   #     print("  A B C D E F G H")
+   #     print("  +-+-+-+-+-+-+-+")
+   #     row_number = 1
+    #    for row in self.board:
+     #       print("%d|%s|" % (row_number, "|".join(row)))
+     #       row_number += 1
+
+#class battleship:
+
+menu()
